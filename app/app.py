f1c03b60d27a18a1e6cbafe3553984dece7e8840
Florian Chávez-Juárez <florian.chavez@coreso.ch>
2024-10-31 11:41:39 +0100

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd 
+
+
+
+st.header("Hello world")
+
+st.markdown("I am looking forward to programming  and then this app...")
+
+
+yourname = st.selectbox("hi",["A","b"])
+
+st.markdown(yourname)
